94a37f12665e78c82a5f42006f82a5e7507f14e5
GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAG <seogaeun@tukorea.ac.kr>
2023-09-08 15:19:55 +0900

    5번 코드 절반 작성

--- 5.cpp
+++ 5.cpp
@@ -1,222 +1,224 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <stdio.h>
+#include <random>
+#include <Windows.h>
+#include <conio.h>
+
+#define UP 72
+#define DOWN 80
+#define RIGHT 77
+#define LEFT 75
 
 using namespace std;
 
-struct storeList
-{
-	bool full = false;
-	int x, y, z;
-};
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<int> dis(1, 4);
 
-storeList List[10];
-storeList copyList[10];
-storeList sList[10];
+struct ARR
+{
+	char aa;	//숨은 알파벳
+	char a;	//드러난 문자
+	bool isSelection = false;
+	bool isFull = false;
+	bool cursor = false;
+};
 
 int main()
 {
-	char order;
-	int X, Y, Z;
-	int q=1;
-	int cnt = 0;
-	int s = 0;
-	bool Select = false;
-
-	while (q)
+	ARR arr[5][5];
+	
+	//초기화
+	for (int i = 0; i < 5; i++)
 	{
-		printf("명령어를 입력하시오. : ");
-		scanf("%s", &order);
+		for (int j = 0; j < 5; j++)
+		{
+			arr[i][j].a = '*';
+		}
+	}
 
-		switch (order)
+	arr[0][0].a = ' ';
+	arr[0][1].a = 'a';
+	arr[0][2].a = 'b';
+	arr[0][3].a = 'c';
+	arr[0][4].a = 'd';
+
+	arr[1][0].a = '1';
+	arr[2][0].a = '2';
+	arr[3][0].a = '3';
+	arr[4][0].a = '4';
+
+	//랜덤 문자 넣기
+	int M, K;
+	int put = 0;	//0,1 = 'A' 2,3 = 'B', 4,5 = 'C'...
+	while (put != 16)
+	{
+		M = dis(gen);
+		K = dis(gen);
+
+		if (put < 2 && !arr[M][K].isFull)
+		{
+			arr[M][K].aa = 'A';
+			arr[M][K].isFull = true;
+			put++;
+		}
+		if (put >= 2 && put < 4 && !arr[M][K].isFull)
+		{
+			arr[M][K].aa = 'B';
+			arr[M][K].isFull = true;
+			put++;
+		}
+		if (put >= 4 && put < 6 && !arr[M][K].isFull)
+		{
+			arr[M][K].aa = 'C';
+			arr[M][K].isFull = true;
+			put++;
+		}
+		if (put >= 6 && put < 8 && !arr[M][K].isFull)
+		{
+			arr[M][K].aa = 'D';
+			arr[M][K].isFull = true;
+			put++;
+		}
+		if (put >= 8 && put < 10 && !arr[M][K].isFull)
+		{
+			arr[M][K].aa = 'E';
+			arr[M][K].isFull = true;
+			put++;
+		}
+		if (put >= 10 && put < 12 && !arr[M][K].isFull)
+		{
+			arr[M][K].aa = 'F';
+			arr[M][K].isFull = true;
+			put++;
+		}
+		if (put >= 12 && put < 14 && !arr[M][K].isFull)
+		{
+			arr[M][K].aa = 'G';
+			arr[M][K].isFull = true;
+			put++;
+		}
+		if (put >= 14 && put < 16 && !arr[M][K].isFull)
+		{
+			arr[M][K].aa = 'H';
+			arr[M][K].isFull = true;
+			put++;
+		}
+	}
+
+	/*for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			cout <<' ' << arr[i][j].a;
+		}
+		cout << endl;
+	}*/
+
+	char input;
+	int I = 1;
+	int J = 1;
+	int selection = 0;	//2면 초기화
+
+	while (1)
+	{
+		system("cls");
+		//출력
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 5; j++)
+			{
+				cout << ' ' << arr[i][j].a;
+			}
+			cout << endl;
+		}
+
+		//키보드 입력 받기
+		input = _getch();
+		
+		switch (input)
 		{
 		case '.':
-			for (int i = 9; i >= 0; i--)
-			{
-				if (List[i].full)
-				{
-					printf("%d번째 칸 : %d %d %d\n", i, List[i].x, List[i].y, List[i].z);
-				}
-				else
-				{
-					printf("%d번째 칸 :      \n", i);
-				}
-			}
+			arr[I][J].a = arr[I][J].aa;
+			arr[I][J].isSelection = true;
 			break;
-		case '+':
-			scanf("%d %d %d",&X, &Y, &Z);
-
-			for (int i = 9; i >= 0; i--)
-			{
-				if (List[9].full)
-				{
-					for (int i = 0; i < 10; i++)
-					{
-						if (!List[i].full)
-						{
-							List[i].full = true;
-							List[i].x = X;
-							List[i].y = Y;
-							List[i].z = Z;
-							break;
-						}
-					}
-					break;
-				}
-				else if (List[i].full)
-				{
-					List[i + 1].full = true;
-					List[i + 1].x = X;
-					List[i + 1].y = Y;
-					List[i + 1].z = Z;
-					break;
-				}
-				else if(!List[0].full)
-				{
-					List[0].full = true;
-					List[0].x = X;
-					List[0].y = Y;
-					List[0].z = Z;
-					break;
-				}
-			}
-			break;
-		case '-':
-			for (int i = 9; i >= 0; i--)
-			{
-				if (List[i].full)
-				{
-					List[i].full = false;
-					List[i].x = 0;
-					List[i].y = 0;
-					List[i].z = 0;
-					break;
-				}
-			}
-			break;
-		case 'e':
-			scanf("%d %d %d", &X, &Y, &Z);
+		case LEFT:
 			
-			for (int i = 0; i < 10; i++)
+			if (J > 1)
 			{
-				copyList[i] = List[i];
-			}
-
-			for (int i = 0; i < 9; i++)
-			{
-				List[i+1] = copyList[i];
-			}
-
-			List[0].full = true;
-			List[0].x = X;
-			List[0].y = Y;
-			List[0].z = Z;
-
-			break;
-		case 'd':
-			for (int i = 0; i < 10; i++)
-			{
-				if (List[i].full)
+				for (int i = 1; i < 5; i++)
 				{
-					List[i].full = false;
-					List[i].x = 0;
-					List[i].y = 0;
-					List[i].z = 0;
-					break;
-				}
-			}
-			break;
-		case 'l':
-			cnt = 0;
-
-			for (int i = 0; i < 10; i++)
-			{
-				if (List[i].full)
-				{
-					cnt++;
-				}
-			}
-			printf("리스트 길이 : %d\n", cnt);
-			break;
-		case 'c':
-			for (int i = 0; i < 10; i++)
-			{
-				List[i].full = false;
-				List[i].x = 0;
-				List[i].y = 0;
-				List[i].z = 0;
-			}
-			break;
-		case 'm':
-			for (int i = 9; i >= 0; i--)
-			{
-				if (List[i].full)
-				{
-					printf("(%d %d %d)\n", List[i].x, List[i].y, List[i].z);
-					break;
-				}
-			}
-			break;
-		case 'n':
-			for (int i = 0; i < 10; i++)
-			{
-				if (List[i].full)
-				{
-					printf("(%d %d %d)\n", List[i].x, List[i].y, List[i].z);
-					break;
-				}
-			}
-			break;
-		case 's':
-			s = 0;
-			Select = !Select;
-
-			if (Select)
-			{
-				for (int i = 0; i < 10; i++)
-				{
-					copyList[i] = List[i];
-				}
-
-				for (int i = 0; i < 10; i++)
-				{
-					List[i].full = false;
-					List[i].x = 0;
-					List[i].y = 0;
-					List[i].z = 0;
-				}
-
-				for (int i = 0; i < 10; i++)
-				{
-					if (s > 9)
+					for (int j = 1; j < 5; j++)
 					{
-						break;
-					}
-					else if (copyList[s].full)
-					{
-						List[i] = copyList[s];
-						s++;
-					}
-					else if (!copyList[s].full)
-					{
-						i--;
-						s++;
+						arr[i][j].cursor = false;
 					}
 				}
+				J--;
+				arr[I][J].cursor = true;
 			}
-
-			else
-			{
-				for (int i = 0; i < 10; i++)
-				{
-					List[i] = copyList[i];
-				}
-			}
+			break;
+		case RIGHT:
 			
+			if (J <4)
+			{
+				for (int i = 1; i < 5; i++)
+				{
+					for (int j = 1; j < 5; j++)
+					{
+						arr[i][j].cursor = false;
+					}
+				}
+				J++;
+				arr[I][J].cursor = true;
+			}
 			break;
-		case'q':
-			q = 0;
+		case UP:
+			
+			if (I > 1)
+			{
+				for (int i = 1; i < 5; i++)
+				{
+					for (int j = 1; j < 5; j++)
+					{
+						arr[i][j].cursor = false;
+					}
+				}
+				I--;
+				arr[I][J].cursor = true;
+			}
 			break;
+		case DOWN:
+			
+			if (I < 4)
+			{
+				for (int i = 1; i < 5; i++)
+				{
+					for (int j = 1; j < 5; j++)
+					{
+						arr[i][j].cursor = false;
+					}
+				}
+				I++;
+				arr[I][J].cursor = true;
+			}
+			break;
+		}
+
+		//키보드 커서 확인
+		for (int i = 1; i < 5; i++)
+		{
+			for (int j = 1; j < 5; j++)
+			{
+				if (arr[i][j].cursor && !arr[i][j].isSelection)
+				{
+					arr[i][j].a = 'O';
+				}
+				if (!arr[i][j].cursor && !arr[i][j].isSelection)
+				{
+					arr[i][j].a = '*';
+				}
+			}
 		}
 	}
 }
